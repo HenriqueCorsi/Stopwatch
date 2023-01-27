358af13c16a1d6ec81dc65539cc53eb7804e4124
Paulo Henrique <ph55@hotmail.com>
2023-01-27 17:06:43 -0300

    Estrutura Básica

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    var seconds = 0;
+    var minutes = 0;
+    var appendMinutes = document.getElementById('minutes');
+    var appendSeconds = document.getElementById('seconds');
+    var buttonStart = document.getElementById('button-start');
+    var buttonStop = document.getElementById('button-stop');
+    var buttonReset = document.getElementById('button-reset');
+    var Interval;
+
+    
+}
